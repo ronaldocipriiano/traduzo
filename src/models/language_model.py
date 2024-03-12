0680e6813c1a4544e4377e71de040b558f50c148
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-03-12 16:44:59 -0300

    Implementando a model language_model.py

---
+++ src/models/language_model.py
@@ -0,0 +1,21 @@
+from src.database.db import db
+from src.models.abstract_model import AbstractModel
+
+
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def save(self):
+        result = super().save()
+        return result
+
+    def to_dict(self):
+        return {key: value for key, value in self.data.items() if key != "_id"}
+
+    @classmethod
+    def list_dicts(cls):
+        languages = cls._collection.find({}, {"_id": 0})
+        return [language for language in languages]
