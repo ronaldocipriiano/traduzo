8f9d0c4fb21daca796d0c89c8b2fb708d83a44f6
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-03-18 10:42:28 -0300

    Implementando TESTE - Exclusão de Histórico de Traduções - DELETE

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,42 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
+
+
+def user():
+    UserModel(
+        {
+            "name": "Peter",
+            "level": "admin",
+            "token": "token",
+        }
+    ).save()
+
+    return {"name": "Peter", "level": "admin", "token": "token"}
+
+
+def history():
+    history_instance = HistoryModel(
+        {
+            "text_to_translate": "text",
+            "translate_from": "pt",
+            "translate_to": "en",
+        }
+    )
+
+    history_instance.save()
+    return history_instance.data
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    user_data = user()
+    history_data = history()
+
+    response = app_test.delete(
+        f"/admin/history/{str(history_data['_id'])}",
+        headers={
+            "Authorization": user_data["token"],
+            "User": user_data["name"]
+        },
+    )
+
+    assert response.status_code == 204
